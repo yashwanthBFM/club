a92022f7ba8171b04000a69c48f00c9e0dea6ae4
Pallela1907 <125499371+Pallela1907@users.noreply.github.com>
2025-06-17 17:22:13 +0530

    parallax-design

--- apps/user-web/src/app/Info.tsx
+++ apps/user-web/src/app/Info.tsx
@@ -3,10 +3,8 @@ import Link from 'next/link';
 
 export default function Info() {
   return (
-    <>
+<section className={`${styles.infoSection} ${styles.parallaxTeamSection}`}>
 
-    
-    <section className={`${styles.infoSection} ${styles.parallaxTeamSection}`}>
       <h1 className={styles.infoTitle}>Meet Our Team</h1>
       {/* First Block: Text Left, Image Right */}
       <div className={styles.infoBlock} data-card-type="coach">
@@ -14,7 +12,7 @@ export default function Info() {
           <h2>Coach James Carter – Head Coach</h2>
           <p>Coach James brings over 12 years of coaching experience across youth academies and semi-professional clubs in Europe. Known for his strategic mindset and player-first approach, he focuses on developing intelligent, adaptable players who understand the game beyond the ball. His training sessions are intense, purposeful, and rooted in real match scenarios.</p>
         </div>
-        <img src="/coach_2.jpg" alt="Game Scene 1" className={styles.coachImage} />
+        <img src="/coach_1.jpg" alt="Game Scene 1" className={styles.coachImage} />
       </div>
 
       {/* Second Block: Image Left, Text Right */}
@@ -23,7 +21,7 @@ export default function Info() {
           <h2>Coach Lucas Bennett – </h2>
           <p>Lucas is the driving force behind the squad's speed, stamina, and skill. With a background in sports physiology and elite training methods, he pushes players to unlock their athletic and technical potential. His high-energy sessions are designed to simulate real-game pressure while sharpening footwork, reaction time, and overall fitness.</p>
         </div>
-        <img src="/coach_1.jpg" alt="Game Scene 2" className={styles.coachImage} />
+        <img src="/coach_2.jpg" alt="Game Scene 2" className={styles.coachImage} />
       </div>
 
       <div className={styles.teamButtonWrapper}>
@@ -34,7 +32,6 @@ export default function Info() {
 
     </section>
 
-</>
     
   );
 }
